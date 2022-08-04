cb32338eca082066f73936d2469685416413b941
iejMac <kilianmaciej6@gmail.com>
2022-08-04 15:33:05 +0000

    finished script

--- workflow/vid_dataset/taskset_get_arrs.py
+++ workflow/vid_dataset/taskset_get_arrs.py
@@ -1,5 +1,6 @@
 import os
 import psutil
+import tempfile
 import numpy as np
 
 from multiprocessing import Process
@@ -8,26 +9,45 @@ from multiprocessing import Process
 def run_proc(cmd):
     os.system(cmd)
 
-SPLIT = 2
+SPLIT = 3
+URLS = "urls.txt" # txt file with urls to download and process as lines
+# Split up URLS:
+with open(URLS, "r") as f:
+    lines = f.read().splitlines()
 
-cpus = psutil.Process().cpu_affinity()
-split = [s.tolist() for s in np.array_split(cpus, SPLIT)]
-strings = []
-for cpu_list in split:
-    str_cpu_list = [str(num) for num in cpu_list]
-    strings.append(",".join(str_cpu_list))
-    
+split_lines = [s.tolist() for s in np.array_split(lines, SPLIT)]
 
-command = "taskset --cpu-list {} video2numpy urls{}.txt --dest arrs/ --take-every-nth 25 --workers 6"
+with tempfile.TemporaryDirectory() as tmpdirname:
+    url_pths = []
+    for i, ls in enumerate(split_lines):
+        url_pth = os.path.join(tmpdirname, f"urls{i}.txt") 
+        with open(url_pth, "w") as f:
+            for line in ls:
+                f.write(line + '\n')
+        url_pths.append(url_pth)
 
-procs = []
-for id_, cpus in enumerate(strings):
-    cmd = command.format(cpus, id_+1)
-    procs.append(Process(args=(cmd,), target=run_proc))
+    cpus = psutil.Process().cpu_affinity()
+    split = [s.tolist() for s in np.array_split(cpus, SPLIT)]
+    strings = []
+    for cpu_list in split:
+        str_cpu_list = [str(num) for num in cpu_list]
+        strings.append(",".join(str_cpu_list))
+        
 
+    command = "taskset --cpu-list {} video2numpy {} --dest arrs/ --take-every-nth 25 --workers {}"
 
-for p in procs:
-    p.start()
+    procs = []
+    for i, cpus in enumerate(strings):
+        cmd = command.format(cpus, url_pths[i], len(cpus.split(",")))
+        print(cmd)
+        procs.append(Process(args=(cmd,), target=run_proc))
 
-for p in procs:
-    p.join()
+    print("STARTING READING...")
+
+    for p in procs:
+        p.start()
+
+    for p in procs:
+        p.join()
+
+    print("DONE READING")
