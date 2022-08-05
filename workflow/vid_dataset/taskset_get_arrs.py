10c25e2af1c2052c4f0b59a445a46f45d228ecc5
iejMac <kilianmaciej6@gmail.com>
2022-08-05 15:05:28 +0000

    add GPU workers

--- workflow/vid_dataset/taskset_get_arrs.py
+++ workflow/vid_dataset/taskset_get_arrs.py
@@ -9,13 +9,14 @@ from multiprocessing import Process
 def run_proc(cmd):
     os.system(cmd)
 
-SPLIT = 3
-URLS = "urls.txt" # txt file with urls to download and process as lines
+SPLIT = 7
+# URLS = "urls1.txt" # txt file with urls to download and process as lines
+URLS = "urls_val.txt" # txt file with urls to download and process as lines
 # Split up URLS:
 with open(URLS, "r") as f:
     lines = f.read().splitlines()
 
-split_lines = [s.tolist() for s in np.array_split(lines, SPLIT)]
+split_lines = [s.tolist() for s in np.array_split(lines, SPLIT - 1)] # one split is for GPU worker
 
 with tempfile.TemporaryDirectory() as tmpdirname:
     url_pths = []
@@ -34,13 +35,18 @@ with tempfile.TemporaryDirectory() as tmpdirname:
         strings.append(",".join(str_cpu_list))
         
 
-    command = "taskset --cpu-list {} video2numpy {} --dest arrs/ --take-every-nth 25 --workers {}"
+    command = "taskset --cpu-list {} video2numpy {} --dest arrs1/ --take-every-nth 25 --workers {}"
+    cpu_strings = strings[:-1]
+    gpu_string = strings[-1]
 
     procs = []
-    for i, cpus in enumerate(strings):
+    for i, cpus in enumerate(cpu_strings):
         cmd = command.format(cpus, url_pths[i], len(cpus.split(",")))
         print(cmd)
         procs.append(Process(args=(cmd,), target=run_proc))
+
+    gpu_cmd = f"taskset --cpu-list {gpu_string} python3.8 live_encoding.py arrs1"
+    procs.append(Process(args=(gpu_cmd,), target=run_proc))
 
     print("STARTING READING...")
 
